5f53f55321a19b810ae111952a05c67c38257c07
ERG <elierrosalesgarcia@gmail.com>
2024-05-13 12:28:24 +0100

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Contador
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Contador();
+        }
+    }
+}
---
+++ Contador.xaml.cs
@@ -0,0 +1,9 @@
+namespace Contador;
+
+public partial class Contador : TabbedPage
+{
+	public Contador()
+	{
+		InitializeComponent();
+	}
+}
